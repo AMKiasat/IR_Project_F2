2e4f70785c1708839b71219fdea84006a61cdcaf
AMKiasat <kiasatamirm@gmail.com>
2021-12-24 18:43:07 +0330

    adding question words tf

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 
 def main():
     question = input("I am your google assistance ask me your question:")
-    position = dict()
+    Repetition = dict()
     df = pd.read_excel('IR1_7k_news.xlsx')
     x = df['content']
     title = df['title']
@@ -23,10 +23,10 @@ def main():
     N = len(x)
     for i in sw:
         stop_words[i] = 1
-    # stemmed = []
-    # if os.path.isfile('position.pickle'):
-    #     with open('position.pickle', 'rb') as handle:
-    #         position = pickle.load(handle)
+    stemmed = []
+    # if os.path.isfile('Repetition.pickle'):
+    #     with open('Repetition.pickle', 'rb') as handle:
+    #         Repetition = pickle.load(handle)
     # else:
     for i in range(N):
         words = my_tokenizer.tokenize_words(my_normalizer.normalize(x[i]))
@@ -35,36 +35,50 @@ def main():
             if words[j] not in stop_words:
                 b = my_stemmer.convert_to_stem(words[j])
                 # tmp.append(b)
-                if b not in position:
-                    position[b] = dict()
-                if i not in position[b]:
-                    position.get(b)[i] = 0
-                temp = position.get(b).get(i)
-                position.get(b)[i] = temp + 1
+                if b not in Repetition:
+                    Repetition[b] = dict()
+                if i not in Repetition[b]:
+                    Repetition.get(b)[i] = 0
+                temp = Repetition.get(b).get(i)
+                Repetition.get(b)[i] = temp + 1
             # stemmed.append(tmp)
-        # with open('position.pickle', 'wb') as handle:
-        #     pickle.dump(position, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        # with open('Repetition.pickle', 'wb') as handle:
+        #     pickle.dump(Repetition, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     tfidf = dict()
-    for i in position.keys():
-        nt = len(position.get(i))
+    for i in Repetition.keys():
+        nt = len(Repetition.get(i))
         idf = math.log10(N / nt)
         tfidf[i] = dict()
-        for j in position.get(i).keys():
-            tfidf.get(i)[j] = (1 + math.log10(position.get(i).get(j))) * idf
+        for j in Repetition.get(i).keys():
+            tfidf.get(i)[j] = (1 + math.log10(Repetition.get(i).get(j))) * idf
 
     question_words = my_tokenizer.tokenize_words(my_normalizer.normalize(question))
     clean_q_words = []
-    q_w_locations = []
+    # q_w_locations = []
+    q_repetition = dict()
+    q_in_tfidf = dict()
     for i in range(len(question_words)):
         if question_words[i] not in stop_words:
             b = my_stemmer.convert_to_stem(question_words[i])
-            if b not in position:
+            if b not in Repetition:
                 print("No match found.")
                 return
-            q_w_locations.append(position[b])
+            if b not in q_repetition:
+                q_repetition[b] = 0
+            q_repetition[b] = q_repetition.get(b) + 1
+            # q_w_locations.append(Repetition[b])
+            q_in_tfidf[b] = tfidf.get(b)
             clean_q_words.append(b)
 
+    q_tf = dict()
+    q_vector_size = 0.0
+    for i in q_repetition.keys():
+        q_tf[i] = (1 + math.log10(q_repetition.get(i)))
+        print(q_repetition.get(i))
+        q_vector_size = q_vector_size + (q_tf.get(i) * q_tf.get(i))
+    q_vector_size = math.sqrt(q_vector_size)
+    
 
 if __name__ == '__main__':
     main()
